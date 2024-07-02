b9fcdc2e52e5edb84d91a16dcd0ad496742ba686
Nihal-10 <nihal.padwal@ting.in>
2024-07-02 14:30:17 +0530

    ui is ready

---
+++ src/app/genre/create/page.js
@@ -0,0 +1,61 @@
+// MODULES //
+
+// COMPONENTS //
+import MetaTags from "@/components/MetaTags";
+
+// SECTIONS //
+
+// PLUGINS //
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+
+import { Save } from "lucide-react";
+import { Input } from "@/components/ui/input";
+
+// STYLES //
+
+// UTILS //
+
+// IMAGES //
+
+// DATA //
+
+/** Contact Page */
+export default function Page() {
+	return (
+		<div>
+			<MetaTags Title={"Contact"} Desc={""} OgImg={""} Url={"/contact"} />
+
+			{/* Page Content starts here */}
+
+			<main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+				<div className="flex justify-end">
+					<Button size="sm" className="h-8 gap-1">
+						<Save className="h-3.5 w-3.5" />
+						<span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Save</span>
+					</Button>
+				</div>
+				<Form />
+			</main>
+			{/* Page Content ends here */}
+		</div>
+	);
+}
+
+/** */
+const Form = () => {
+	return (
+		<div className="relative flex-col items-start gap-8 md:flex">
+			<form className="grid w-full items-start gap-6">
+				<fieldset className="grid gap-6 rounded-lg border p-4">
+					<legend className="-ml-1 px-1 text-sm font-medium">Genre</legend>
+
+					<div className="grid gap-3">
+						<Label htmlFor="temperature">Name</Label>
+						<Input id="firstname" type="text" placeholder="First" />
+					</div>
+				</fieldset>
+			</form>
+		</div>
+	);
+};
---
+++ src/sections/genre/GenreContainer.js
@@ -0,0 +1,135 @@
+// MODULES //
+
+// COMPONENTS //
+import Image from "next/image";
+
+// SECTIONS //
+
+// PLUGINS //
+import { MoreHorizontal } from "lucide-react";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import {
+	Pagination,
+	PaginationContent,
+	PaginationEllipsis,
+	PaginationItem,
+	PaginationLink,
+	PaginationNext,
+	PaginationPrevious,
+} from "@/components/ui/pagination";
+
+// STYLES //
+
+// UTILS //
+
+// IMAGES //
+import ProfileImg from "/public/img/users/profile_pic.png";
+
+// DATA //
+
+/** Genre Container */
+export default function GenreContainer() {
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>Genre</CardTitle>
+				<CardDescription>Manage your genres.</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead>Name</TableHead>
+							{/* <TableHead>Status</TableHead> */}
+							{/* <TableHead className="hidden md:table-cell">Price</TableHead> */}
+							{/* <TableHead className="hidden md:table-cell">Total Sales</TableHead> */}
+							<TableHead className="hidden md:table-cell">Created at</TableHead>
+							<TableHead>
+								<span className="sr-only">Actions</span>
+							</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						<TableRow>
+							<TableCell className="font-medium">Dancing</TableCell>
+							{/* <TableCell>
+								<Badge variant="outline">Draft</Badge>
+							</TableCell> */}
+							{/* <TableCell className="hidden md:table-cell">$499.99</TableCell> */}
+							{/* <TableCell className="hidden md:table-cell">25</TableCell> */}
+							<TableCell className="hidden md:table-cell">
+								2023-07-12 10:42 AM
+							</TableCell>
+							<TableCell>
+								<DropdownMenu>
+									<DropdownMenuTrigger asChild>
+										<Button aria-haspopup="true" size="icon" variant="ghost">
+											<MoreHorizontal className="h-4 w-4" />
+											<span className="sr-only">Toggle menu</span>
+										</Button>
+									</DropdownMenuTrigger>
+									<DropdownMenuContent align="end">
+										<DropdownMenuLabel>Actions</DropdownMenuLabel>
+										<DropdownMenuItem>Edit</DropdownMenuItem>
+										<DropdownMenuItem>Delete</DropdownMenuItem>
+									</DropdownMenuContent>
+								</DropdownMenu>
+							</TableCell>
+						</TableRow>
+					</TableBody>
+				</Table>
+			</CardContent>
+			{/* <CardFooter>
+				<Pagination>
+					<PaginationContent>
+						<PaginationItem>
+							<PaginationPrevious href="#" />
+						</PaginationItem>
+						<PaginationItem>
+							<PaginationLink href="#">1</PaginationLink>
+						</PaginationItem>
+						<PaginationItem>
+							<PaginationLink href="#" isActive>
+								2
+							</PaginationLink>
+						</PaginationItem>
+						<PaginationItem>
+							<PaginationLink href="#">3</PaginationLink>
+						</PaginationItem>
+						<PaginationItem>
+							<PaginationEllipsis />
+						</PaginationItem>
+						<PaginationItem>
+							<PaginationNext href="#" />
+						</PaginationItem>
+					</PaginationContent>
+				</Pagination>
+			</CardFooter> */}
+		</Card>
+	);
+}
